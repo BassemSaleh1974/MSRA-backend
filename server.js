4815a7ca0f6bff79609aabd593f001a20b78192c
Bassem Saleh <bassemsaleh1974@gmail.com>
2025-03-18 02:31:22 +0000

    Added /about route

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/status', (req, res) => {
     time: new Date()
   });
 });
+app.get('/about', (req, res) => {
+  res.json({
+    project: 'MSRA-backend',
+    developer: 'Bassem',
+    message: 'This is a simple backend built with Express!',
+    time: new Date()
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
